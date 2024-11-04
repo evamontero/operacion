7212045d89ed015f966c03345aae3117a040436c
evamontero <eva.montero.sanjurjo@colexio-karbo.com>
2024-11-04 17:02:19 +0100

    prueba

---
+++ src/main/java/com/mycompany/operacion/Operacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operacion;
+
+/**
+ *
+ * @author 2024-25DAM1
+ */
+public class Operacion {
+
+    public static void main(String[] args) {
+        
+    }
+}
